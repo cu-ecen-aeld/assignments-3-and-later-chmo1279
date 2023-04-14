e326a3e85612f99e566a9dc22a4d0516f0643b1b
Christopher Morgan <chmo1279@colorado.edu>
2023-04-14 01:33:29 -0400

    Double thread test failing

--- examples/threading/threading.c
+++ examples/threading/threading.c
@@ -1,22 +1,27 @@
+#define _GNU_SOURCE
 #include "threading.h"
+#include <pthread.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <errno.h>
 
 // Optional: use these functions to add debug or error prints to your application
 #define DEBUG_LOG(msg,...)
 //#define DEBUG_LOG(msg,...) printf("threading: " msg "\n" , ##__VA_ARGS__)
 #define ERROR_LOG(msg,...) printf("threading ERROR: " msg "\n" , ##__VA_ARGS__)
-
+  
 void* threadfunc(void* thread_param)
 {
-
     // TODO: wait, obtain mutex, wait, release mutex as described by thread_data structure
     // hint: use a cast like the one below to obtain thread arguments from your parameter
     //struct thread_data* thread_func_args = (struct thread_data *) thread_param;
-    return thread_param;
+    struct thread_data* thread_func_args = (struct thread_data*) thread_param;
+    usleep(thread_func_args->wait_to_obtain_ms * 20 * 1000);
+    usleep(thread_func_args->wait_to_release_ms * 20 * 1000);
+    thread_func_args->thread_complete_success = true;
+    return thread_func_args;
 }
-
 
 bool start_thread_obtaining_mutex(pthread_t *thread, pthread_mutex_t *mutex,int wait_to_obtain_ms, int wait_to_release_ms)
 {
@@ -28,6 +33,14 @@ bool start_thread_obtaining_mutex(pthread_t *thread, pthread_mutex_t *mutex,int 
      *
      * See implementation details in threading.h file comment block
      */
-    return false;
-}
+    //pthread_mutex_lock(mutex);
+    struct thread_data *thread_func_data = (struct thread_data *)malloc(sizeof(*thread_func_data));
+//    thread_func_data->thread = *thread;
+    thread_func_data->mutex = *mutex;
+    thread_func_data->wait_to_obtain_ms = wait_to_obtain_ms;
+    thread_func_data->wait_to_release_ms = wait_to_release_ms;
+    int rc = pthread_create(thread, NULL, &threadfunc, thread_func_data);
+    //printf("%d\n",thread_func_data->thread_complete_success); 
+    return (!rc);
 
+}

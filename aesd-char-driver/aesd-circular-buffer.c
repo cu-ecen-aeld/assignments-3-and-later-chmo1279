218bc47aad81d77d39144a3bcec5dfa36a05e60d
Christopher Morgan <chmo1279@colorado.edu>
2023-06-01 00:51:14 -0400

    Reformatted for human eyes

--- aesd-char-driver/aesd-circular-buffer.c
+++ aesd-char-driver/aesd-circular-buffer.c
@@ -16,24 +16,25 @@
 
 #include "aesd-circular-buffer.h"
 
-/**
- * @param buffer the buffer to search for corresponding offset.  Any necessary locking must be performed by caller.
- * @param char_offset the position to search for in the buffer list, describing the zero referenced
- *      character index if all buffer strings were concatenated end to end
- * @param entry_offset_byte_rtn is a pointer specifying a location to store the byte of the returned aesd_buffer_entry
- *      buffptr member corresponding to char_offset.  This value is only set when a matching char_offset is found
- *      in aesd_buffer.
- * @return the struct aesd_buffer_entry structure representing the position described by char_offset, or
- * NULL if this position is not available in the buffer (not enough data is written).
- */
-struct aesd_buffer_entry *aesd_circular_buffer_find_entry_offset_for_fpos(struct aesd_circular_buffer *buffer,
-            size_t char_offset, size_t *entry_offset_byte_rtn )
-{
-    /**
-    * TODO: implement per description
-    */
-    return NULL;
-}
+// struct aesd_buffer_entry
+// {
+//     const char *buffptr; //A location where the buffer contents in buffptr are stored
+//     
+//     size_t size; //Number of bytes stored in buffptr
+// };
+// 
+// struct aesd_circular_buffer
+// {
+//     struct aesd_buffer_entry  entry[AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED];
+//          An array of pointers to memory allocated for the most recent write operations
+//     
+//     uint8_t in_offs; //The current location in the entry structure where the next write should be stored 
+//     
+//     uint8_t out_offs; //The first location in the entry structure to read from
+//     
+//     bool full; //set to true when the buffer entry structure is full
+
+// };
 
 /**
 * Adds entry @param add_entry to @param buffer in the location specified in buffer->in_offs.
@@ -42,17 +43,27 @@ struct aesd_buffer_entry *aesd_circular_buffer_find_entry_offset_for_fpos(struct
 * Any necessary locking must be handled by the caller
 * Any memory referenced in @param add_entry must be allocated by and/or must have a lifetime managed by the caller.
 */
-int i = 0;
-void aesd_circular_buffer_add_entry(struct aesd_circular_buffer *buffer, const struct aesd_buffer_entry *add_entry)
+void aesd_circular_buffer_add_entry(struct aesd_circular_buffer *buffer,
+                                    const struct aesd_buffer_entry *add_entry)
 {
     /**
     * TODO: implement per description
     */
-    if (i <= AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED) {
-    buffer->entry[i]    =   add_entry->buffptr;
-    
-    }
+}
 
+/**
+ *  Finds entry offset for byte position in buffer 
+ *   Any necessary locking must be performed by caller.
+ */
+struct aesd_buffer_entry *aesd_circular_buffer_find_entry_offset_for_fpos(//fpos: byte position in buffer
+                            struct aesd_circular_buffer *buffer, //target buffer to search for corresponding offset.
+                            size_t char_offset, //position to search for in buffer list, describing the zero reference character index if all buffer strings were concatenated end to end (start position?)
+                            size_t *entry_offset_byte_rtn )//pointer specifying a location to store the byte of the returned aesd_buffer_entry buffptr member corresponding to char_offset. Only set when a matching char_offset is found in buffer.
+{
+    /**
+    * TODO: implement per description
+    */
+    return NULL;//return the struct aesd_buffer_entry which represents the location described by char_offset, or NULL if this position is not available in the buffer (not enough data is written).
 }
 
 /**
